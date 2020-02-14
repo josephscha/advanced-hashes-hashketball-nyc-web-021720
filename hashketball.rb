a1f515bb9b7cba2febe5d1ad734adb0658bbca4b
Joseph S. Cha <josephshincha@gmail.com>
2020-02-14 10:10:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -202,14 +202,15 @@ end
 
 def most_points_scored
   points = 0 
+  player = "place holder"
     game_hash.each do |team, team_info|
       team_info[:players].each do |player|
         if player[:points] > points 
           points = player[:points]
-        
+          player = player[:player_name]
         end
       end
     end
-    return player[:player_name]
+
   end
 
